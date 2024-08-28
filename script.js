89d6f36f4b485dc8c774c7c552560b13105e3df3
saberloty <sabertayyeb@gmail.com>
2024-08-28 23:46:58 +0330

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
       appToken: "d1690a07-3780-4068-810f-9b5bbf2931b2",
       promoId: "b4170868-cef0-424f-8eb9-be0622e8e8e3",
     },
-    3: {
-      name: "My Clone Army",
-      appToken: "74ee0b5b-775e-4bee-974f-63e7f4d5bacb",
-      promoId: "fe693b26-b342-4159-8808-15e3ff7f8767",
-    },
     4: {
       name: "Train Miner",
       appToken: "82647f43-3f87-402d-88dd-09a90025313f",
@@ -46,8 +41,13 @@ document.addEventListener("DOMContentLoaded", () => {
         name: 'Mud Racing',
         appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
         promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
-    }
-  };
+},
+        10: {
+            name: 'Cafe Dash',
+            appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+            promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11', 
+        }
+    };
 
   const gameOptions = document.querySelectorAll(".game-option");
   const keyCountGroup = document.getElementById("keyCountGroup");
